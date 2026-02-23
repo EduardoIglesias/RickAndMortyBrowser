32480a03b8e77738684c16fe25aeb980d9677244
Eduardo Iglesias Fernandez <EduardoIglesias>
2026-02-23 14:11:41 +0100

    Initial Commit

---
+++ RickAndMortyBrowser/RickAndMortyBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyBrowserApp.swift
+//  RickAndMortyBrowser
+//
+//  Created by Eduardo Iglesias Fernandez on 23/2/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyBrowserTests/RickAndMortyBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyBrowserTests.swift
+//  RickAndMortyBrowserTests
+//
+//  Created by Eduardo Iglesias Fernandez on 23/2/26.
+//
+
+import Testing
+@testable import RickAndMortyBrowser
+
+struct RickAndMortyBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
